6b30d3a89b10fc2108865d72b526a6f5e662b1a8
achao <>
2025-06-02 16:51:08 +0800

    mysql class study

--- python/bottle/README.md
+++ python/bottle/README.md
@@ -1,3 +1,18 @@
-python bootle study
 # 1. python 连接数据库
 数据库依赖 mysql-connector-python
+
+```python
+import mysql.connector
+conn = mysql.connetor.connect(
+    host='0.0.0.0', # 数据库地址
+    port=3306,  # 端口
+    database='database',    #数据库
+    user='user',    #用户
+    password='password' #密码
+)
+cursor = conn.cursor    #获取操作游标
+
+cursor.execute('select * from table')   # 构建操作指令
+cursor.fetchall()   #如果是查询，拉取查询结果
+conn.commit() # 如果是插入，更新，删除等操作
+```
--- python/bottle/database/__init__.py
+++ python/bottle/database/__init__.py
@@ -1,17 +1,52 @@
+import this
+
 import mysql.connector
 
-# 创建连接
-conn = mysql.connector.connect(
-    host='127.0.0.1',
-    user='root',
-    password='123456',
-    database='pet'
-)
-# 获取 mysql 操作游标
-cursor =conn.cursor()
 
-#构造查询
-cursor.execute('select * from user')
-# 获取查询结果
-results = cursor.fetchall()
-print(results)
+class DataConnect:
+    def __init__(self, host='127.0.0.1', user='root', passwd='123456', database='pet'):
+        # 数据库 连接
+        self.conn = mysql.connector.connect(host=host, user=user, passwd=passwd, database=database)
+        # 获取操作游标
+        self.cursor = self.conn.cursor()
+    def selectData(self, query,val=(),size=0):
+        self.cursor.execute(query,val)
+        if size == 0:
+           return self.cursor.fetchall()
+        if size == 1:
+            return self.cursor.fetchone()
+        else:
+            return self.cursor.fetchmany(size)
+
+    def commitData(self):
+        self.conn.commit()
+
+    def addData(self, query,val=()):
+        self.cursor.execute(query,val)
+        self.conn.commit()
+
+
+    def deleteData(self, query,val=()):
+        self.cursor.execute(query,val)
+        self.conn.commit()
+
+
+    def updateData(self, query,val=()):
+        self.cursor.execute(query,val)
+        self.conn.commit()
+    def queryData(self, query,val=(),size=0):
+        if 'select' in query:
+            raise Exception('查询操作请使用 selectData')
+        self.cursor.execute(query,val)
+        self.conn.commit()
+if __name__ == '__main__':
+    db = DataConnect()
+    print(db.selectData('select * from user'))
+    # 添加
+    # db.addData('insert into user(user_id,password,role) values (%s,%s,%s)',(1456,'sdfsdf','OWNER'))
+    # 删除
+    # db.deleteData('delete from user where user_id=%s',(1456,))
+    # 更新
+    # db.updateData('update user set username=%s where user_id=%s',('ac',465465))
+    # 抛异常
+    db.queryData('select * from user')
