430da340f48987530045af9e09cbff393dc08fb5
achao <>
2025-06-04 09:22:15 +0800

    gitlab

--- python/bottle/database/__init__.py
+++ python/bottle/database/__init__.py
@@ -9,10 +9,11 @@ class DataConnect:
         self.conn = mysql.connector.connect(host=host, user=user, passwd=passwd, database=database)
         # 获取操作游标
         self.cursor = self.conn.cursor()
-    def selectData(self, query,val=(),size=0):
-        self.cursor.execute(query,val)
+
+    def selectData(self, query, val=(), size=0):
+        self.cursor.execute(query, val)
         if size == 0:
-           return self.cursor.fetchall()
+            return self.cursor.fetchall()
         if size == 1:
             return self.cursor.fetchone()
         else:
@@ -21,32 +22,44 @@ class DataConnect:
     def commitData(self):
         self.conn.commit()
 
-    def addData(self, query,val=()):
-        self.cursor.execute(query,val)
+    def addData(self, query, val=()):
+        self.cursor.execute(query, val)
         self.conn.commit()
 
-
-    def deleteData(self, query,val=()):
-        self.cursor.execute(query,val)
+    def deleteData(self, query, val=()):
+        self.cursor.execute(query, val)
         self.conn.commit()
 
-
-    def updateData(self, query,val=()):
-        self.cursor.execute(query,val)
+    def updateData(self, query, val=()):
+        self.cursor.execute(query, val)
         self.conn.commit()
-    def queryData(self, query,val=(),size=0):
+
+    def queryData(self, query, val=(), size=0):
         if 'select' in query:
             raise Exception('查询操作请使用 selectData')
-        self.cursor.execute(query,val)
+        self.cursor.execute(query, val)
         self.conn.commit()
+
+
+# if __name__ == '__main__':
+#     db = DataConnect()
+#     print(db.selectData('select * from user'))
+# 添加
+# db.addData('insert into user(user_id,password,role) values (%s,%s,%s)',(1456,'sdfsdf','OWNER'))
+# 删除
+# db.deleteData('delete from user where user_id=%s',(1456,))
+# 更新
+# db.updateData('update user set username=%s where user_id=%s',('ac',465465))
+# 抛异常
+# db.queryData('select * from user')
+
+
+def jc(flag):
+    if flag > 1:
+        return flag * jc(flag - 1)
+    else:
+        return flag
+
+
 if __name__ == '__main__':
-    db = DataConnect()
-    print(db.selectData('select * from user'))
-    # 添加
-    # db.addData('insert into user(user_id,password,role) values (%s,%s,%s)',(1456,'sdfsdf','OWNER'))
-    # 删除
-    # db.deleteData('delete from user where user_id=%s',(1456,))
-    # 更新
-    # db.updateData('update user set username=%s where user_id=%s',('ac',465465))
-    # 抛异常
-    db.queryData('select * from user')
+    print(jc(int(input('阶乘目标'))))
